ee1c90891704112582b5929b2ae0dbad44b759d2
TaCeiN <nikolay.krutykh.sss@gmail.com>
2025-08-29 00:48:19 +0300

    Start commit

---
+++ main.py
@@ -0,0 +1,25 @@
+
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv("API_KEY"),
+)
+
+# История сообщений
+messages = [
+    {"role": "system", "content": "Ты помощник по юридическим делам, отвечай кратко и по делу"},
+    {"role": "user", "content": "Привет!"},
+    {"role": "assistant", "content": "Привет! Рад помочь."},
+    {"role": "user", "content": "Кто  ты?"}
+]
+
+completion = client.chat.completions.create(
+    model="openai/gpt-oss-20b:free",
+    messages=messages
+)
+
+print(completion.choices[0].message.content.split("assistantfinal")[-1].strip())
